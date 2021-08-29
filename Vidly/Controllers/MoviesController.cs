e4e2f8ca78a1d53fb2249a4cc70c628904fc02e2
Raphael Rockus <raphaelrockus@gmail.com>
2021-08-29 11:11:10 +0200

    Exercice Section 2 Solution

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -8,7 +8,47 @@ using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
-    public class MoviesController : Controller
+        public class MoviesController : Controller
+        {
+            public ViewResult Index()
+            {
+                var movies = GetMovies();
+
+                return View(movies);
+            }
+
+            private IEnumerable<Movie> GetMovies()
+            {
+                return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+            }
+
+            // GET: Movies/Random
+            public ActionResult Random()
+            {
+                var movie = new Movie() { Name = "Shrek!" };
+                var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
+
+                var viewModel = new RandomMovieViewModel
+                {
+                    Movie = movie,
+                    Customers = customers
+                };
+
+                return View(viewModel);
+            }
+        }
+ }
+
+    //Before exercice
+    /*public class MoviesController : Controller
     {
         // GET: Movies/Random
         public ViewResult Random()
@@ -60,5 +100,4 @@ namespace Vidly.Controllers
         {
             return Content(year + "/" + month);
         }
-    }
-}
+    }*/
