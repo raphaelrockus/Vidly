fa3a293a943b8bc79d8ac55c8086cb3787bd40da
Raphael Rockus <raphaelrockus@gmail.com>
2021-12-22 16:24:24 +0100

    Add Dropdown list to New.cshmtl

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
 using System.Data.Entity;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -22,7 +23,12 @@ namespace Vidly.Controllers
 
         public ActionResult New()
         {
-            return View();
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new NewCustomerViewModel
+            {
+                MemebershipTypes = membershipTypes
+            };
+            return View(viewModel);
         }
 
         public ViewResult Index()
