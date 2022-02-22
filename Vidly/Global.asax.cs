8c0f776426886ed1affba4ff691789196d616bdc
Raphael Rockus <raphaelrockus@gmail.com>
2022-02-22 09:33:43 +0100

    Create Asp.Net Web Api CustomersController

--- Vidly/Global.asax.cs
+++ Vidly/Global.asax.cs
@@ -2,9 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Routing;
 using System.Web.Mvc;
 using System.Web.Optimization;
-using System.Web.Routing;
+using System.Web.Http;
 
 namespace Vidly
 {
@@ -12,6 +13,7 @@ namespace Vidly
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
